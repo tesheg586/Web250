2d04368e024953f916be54e095d02296975d5ee5
tesheg586 <77525041+tesheg586@users.noreply.github.com>
2021-05-03 17:52:01 -0400

    consol

---
+++ consol.php
@@ -0,0 +1,41 @@
+DOCTYPE html>
+<html lang="en"> 
+<head>   
+ 
+        <meta charset="UTF-8"/> 
+       
+        <title>Teshale Wossoro || WEB250 - Brand</title>
+         <link rel="stylesheet" type="text/css" href="styles/brand.css">
+</head>
+ <body>
+         
+       <div class="wrapper"> 
+        <div data-include="components/header.html"></div> 
+ <main>
+
+
+<section>
+
+  
+  <article>
+    <h1>Codeplay</h1>
+    <p></p>
+      <video width = "640" height = "480" controls>
+        <source src = "media/codeplay.mov">
+        Your browser does not support the video.
+      </video>
+    <p></p>
+  </article>
+</section>
+
+<footer>
+  <p>Copyright: &copy; 2021 Aris Digital Agency.  All rights reserved.</p>
+  <br/>
+        <a href="http://validator.w3.org/check?uri=referer" class="myButton">HTML
+        </a>
+        <a href="http://jigsaw.w3.org/css-validator/check/referer" class="myButton">CSS
+        </a>
+</footer>
+
+</body>
+</html>
